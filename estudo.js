52c70003d19b3f6d7b719928f938dc851eb5bf5c
DaviVieiraRodrigues <115164002+DaviVieiraRodrigues@users.noreply.github.com>
2022-10-06 11:10:03 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,26 @@
+let tabuada = 5;
+
+function escreva(){
+    document.write("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+}
+ 
+let lista = ["Jesus","Spessato","Jorge","Duarte","Waldete","Raquel"];
+
+function mostra(){
+    document.write(lista.lenght + "<br>");
+    for(let i = o; i < lista.length; i++){
+        document.write(lista[i] + "<br>");
+    }
+    document.write(lista[0] + "<br>");
+}
 
